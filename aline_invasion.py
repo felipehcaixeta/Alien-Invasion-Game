45881406869315eaed5ca8c2efbbcecd82fce666
Felipe Caixeta <felipehcaixeta@gmail.com>
2025-04-16 13:20:13 -0500

    Set space background image.

--- aline_invasion.py
+++ aline_invasion.py
@@ -8,8 +8,17 @@ class AlienInvasion:
         pygame.init()
         
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption('Alien Invasion')
+
+        # Set the background image.
+        original_bg_img = pygame.image.load("images/background_img_alien_inv.png")
+        self.bg_img = pygame.transform.smoothscale(
+            original_bg_img, (self.screen_width, self.screen_height)
+        )
+
     
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -18,6 +27,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redraw the screen during each pass through the loop.
+            # self.screen.fill([230, 230, 230])
+            self.screen.blit(self.bg_img, (0, 0))
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
