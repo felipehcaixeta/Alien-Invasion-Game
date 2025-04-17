dcf6ccadafe5d52959237a817d2fa69edb6c8919
Felipe Caixeta <felipehcaixeta@gmail.com>
2025-04-17 10:33:19 -0500

    Add fullscreen mode (still incomplete)

--- settings.py
+++ settings.py
@@ -6,14 +6,39 @@ class Settings:
     def __init__(self):
         '''Initialize the game settings.'''
         # Set screen size
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.default_screen_width = 1200
+        self.default_screen_height = 800
+        self.screen_width = self.default_screen_width
+        self.screen_height = self.default_screen_height
+        self.fullscreen = False  # Keep track of fullscreen mode
 
         # Set the background image.
-        original_bg_img = pygame.image.load("images/background_img_alien_inv.png")
+        self.original_bg_img = pygame.image.load("images/background_img_alien_inv.png")
         self.bg_img = pygame.transform.smoothscale(
-            original_bg_img, (self.screen_width, self.screen_height)
+            self.original_bg_img, (self.screen_width, self.screen_height)
         )
 
         # Ship settings
         self.ship_speed = 2
+
+    def _toggle_fullscreen(self):
+        '''Toggle between fullscreen and windowed mode'''
+        if self.fullscreen:
+            self.screen = pygame.display.set_mode(
+            (self.default_screen_width, self.default_screen_height)
+        )
+            self.screen_width = self.default_screen_width
+            self.screen_height = self.default_screen_height
+            self.fullscreen = False
+        else:
+            # Set fullscreen
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.screen_width = self.screen.get_rect().width
+            self.screen_height = self.screen.get_rect().height
+            self.fullscreen = True
+
+        self.bg_img = pygame.transform.smoothscale(
+            self.original_bg_img, (self.screen_width, self.screen_height)
+        )
+        
+        
