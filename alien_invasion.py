0a1335d8aa46f82c68ca35e6de9cca296e35012a
Felipe Caixeta <felipehcaixeta@gmail.com>
2025-04-23 12:51:44 -0500

    Implement level system.

--- alien_invasion.py
+++ alien_invasion.py
@@ -95,6 +95,9 @@ class AlienInvasion:
         '''Start a new game when the player clicks Play'''
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Reset game settings
+            self.settings.initialize_dynamic_settings()
+            
             # Reset game statistics
             self.stats.reset_stats()
             self.game_active = True
@@ -137,6 +140,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet
             self.bullets.empty()
             self._create_alien()
+            self.settings.increase_speed()
     
     def _update_aliens(self):
         '''Check if the fleet is at an edge, then update positions'''
--- settings.py
+++ settings.py
@@ -34,7 +34,24 @@ class Settings:
         self.alien_speed = 1.5
         self.fleet_drop_speed = 20
         # fleet_direction of 1 represents right; -1 represents left
-        self.fleet_direction = 1
+        # self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        '''Initialize settings that change throughout the game'''
+        self.ship_speed = 2
+        self.bullet_speed = 3
+        self.alien_speed = 1.5
+
+    def increase_speed(self):
+        '''Increase speed settings'''
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
 
     def _toggle_fullscreen(self):
         '''Toggle between fullscreen and windowed mode'''
