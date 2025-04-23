1c8faebdd9d23409582e760e27637fde7cf58934
Felipe Caixeta <felipehcaixeta@gmail.com>
2025-04-23 15:22:12 -0500

    Remove buggy ship count

--- alien_invasion.py
+++ alien_invasion.py
@@ -105,7 +105,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
-            self.sb.prep_ships()
+            # self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens
@@ -175,7 +175,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships_left and update scoreboard
             self.stats.ships_left -= 1
-            self.sb.prep_ships()
+            # self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens
             self.bullets.empty()
